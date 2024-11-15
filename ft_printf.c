8f0baf8af8f225a5b0826fa22ba2b51645aeab34
Caio De Souza Marinho <caide-so@c2r2p2.42sp.org.br>
2024-11-15 18:08:59 -0300

    feature: add basic working structure
    add working Makefile, header file and ft_printf file

--- ft_printf.c
+++ ft_printf.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 21:11:51 by caide-so          #+#    #+#             */
-/*   Updated: 2024/11/15 18:05:00 by caide-so         ###   ########.fr       */
+/*   Created: 2024/11/15 17:13:05 by caide-so          #+#    #+#             */
+/*   Updated: 2024/11/15 18:04:28 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
-int	main(void)
+int	ft_printf(const char *format, ...);
+
+int	ft_printf(const char *format, ...)
 {
-	int	teste = ft_printf("a");
-	int	res = 13;
-	if (teste == res)
-		printf("ok\n");
-	else
-		printf("se fudeu\n");	
-	return (0);
+	int	count;
+	int	i;
+	
+	i = 0;
+	while (format[i] != '\0')
+	{
+		count = 0;
+		i++;
+	}
+	count = 13;
+	return (count);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 21:11:51 by caide-so          #+#    #+#             */
-/*   Updated: 2024/11/15 18:05:00 by caide-so         ###   ########.fr       */
+/*   Created: 2024/11/15 17:33:10 by caide-so          #+#    #+#             */
+/*   Updated: 2024/11/15 17:34:52 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-#include <stdio.h>
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int	main(void)
-{
-	int	teste = ft_printf("a");
-	int	res = 13;
-	if (teste == res)
-		printf("ok\n");
-	else
-		printf("se fudeu\n");	
-	return (0);
-}
+int	ft_printf(const char *format, ...);
+
+#endif 
